34c664bffac1997e8c82a3e4b4fe2d9a5fb4d8eb
Deborah Arellanes <darellanes@yahoo.com>
2021-04-28 21:07:48 -0600

    lab02 wildlife complete

---
+++ StudentLab02/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Deborah Arellanes
+    File Name: script.js
+    Date: 04/28/2021
+*/
+
+
+// jQuery for hero image to consume the header window space
+$(document).ready(function (){
+    $('.hero').height($(window).height());
+});
